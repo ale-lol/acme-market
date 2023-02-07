6b63cc61ce8e1b98f54f59d37608ba650fd1f629
Manuel Alejandro Becerra Castro <alebecerrac96@gmail.com>
2023-02-07 16:15:20 +0100

    validaciones de application

--- src/routes/trip.routes.ts
+++ src/routes/trip.routes.ts
@@ -1,12 +1,18 @@
-import {Router} from 'express'
-import { addTrip, getAllTrips, getTrip, updateTrip, deleteTrip } from '../controllers/trip.controller';
+import { Router } from "express";
+import {
+  addTrip,
+  getAllTrips,
+  getTrip,
+  updateTrip,
+  deleteTrip,
+} from "../controllers/trip.controller";
 
 const router = Router();
 
-router.post('/v1/trip', addTrip)
-router.get('/v1/trip', getAllTrips)
-router.get('/v1/trip/:id', getTrip)
-router.put('/v1/trip/:id', updateTrip)
-router.delete('/v1/trip/:id',deleteTrip)
+router.post("/v1/trip", addTrip);
+router.get("/v1/trip", getAllTrips);
+router.get("/v1/trip/:id", getTrip);
+router.put("/v1/trip/:id", updateTrip);
+router.delete("/v1/trip/:id", deleteTrip);
 
-export default router
+export default router;
