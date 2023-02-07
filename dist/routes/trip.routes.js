0ca6781424ffc18fd04d9e376d6561e64fb11f87
Manuel Alejandro Becerra Castro <alebecerrac96@gmail.com>
2023-02-07 17:07:52 +0100

    validaciones de application

--- dist/routes/trip.routes.js
+++ dist/routes/trip.routes.js
@@ -11,6 +11,6 @@ const router = (0, express_1.Router)();
 router.post("/v1/trips", TripValidation_1.creationValidator, ValidationMiddleware_1.default, trip_controller_1.addTrip);
 router.get("/v1/trips", trip_controller_1.getAllTrips);
 router.get("/v1/trips/:id", trip_controller_1.getTrip);
-router.put("/v1/trips/:id", trip_controller_1.updateTrip);
+router.put("/v1/trips/:id", TripValidation_1.creationValidator, ValidationMiddleware_1.default, trip_controller_1.updateTrip);
 router.delete("/v1/trips/:id", trip_controller_1.deleteTrip);
 exports.default = router;
--- src/routes/trip.routes.ts
+++ src/routes/trip.routes.ts
@@ -16,7 +16,7 @@ const router = Router();
 router.post("/v1/trips",creationValidator,handleValidation, addTrip);
 router.get("/v1/trips", getAllTrips);
 router.get("/v1/trips/:id", getTrip);
-router.put("/v1/trips/:id", updateTrip);
+router.put("/v1/trips/:id",creationValidator,handleValidation, updateTrip);
 router.delete("/v1/trips/:id", deleteTrip);
 
 export default router;
